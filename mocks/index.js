fce744531a2c951c35fd0c5ac806ac0af2eca39d
ja153903 <jaimeabbariao@gmail.com>
2022-07-21 19:07:24 -0400

    feat: adding function to start this

--- mocks/index.js
+++ mocks/index.js
@@ -1,7 +1,12 @@
-if (typeof window === "undefined") {
-  const { server } = require('./server');
-  server.listen();
-} else {
-  const { worker } = require('./browser');
-  worker.start();
+function startMSW() {
+  if (typeof window === 'undefined') {
+    const { server } = require('./server');
+    server.listen();
+  } else {
+    const { worker } = require('./browser');
+    worker.start();
+  }
 }
+
+startMSW();
+
